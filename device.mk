f7f8d115c4363a59d3bbe7dbe9ee2d7c33975c2a
Zane <github@turtledev.nz>
2021-04-08 21:43:15 +1200

    Add board size

--- device.mk
+++ device.mk
@@ -35,10 +35,19 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 TARGET_SCREEN_HEIGHT := 3168
 TARGET_SCREEN_WIDTH := 1440
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
+# Device init scripts
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.recovery.target.rc
+
 PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Inherit from oneplus sm8250-common
 $(call inherit-product, device/oneplus/sm8250-common/common.mk)
 
 # Inherit from vendor blobs
-#$(call inherit-product, vendor/oneplus/instantnoodlep/instantnoodlep-vendor.mk)
+$(call inherit-product, vendor/oneplus/instantnoodlep/instantnoodlep-vendor.mk)
